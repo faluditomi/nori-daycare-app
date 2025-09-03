8c7db3df4f283c26440a7f0f8c5bea2d0ded423d
tomi <tomifaludi@gmail.com>
2025-09-04 01:09:27 +0200

    project setup

---
+++ android/app/src/main/java/com/nori/daycare/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nori.daycare;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nori.daycare',
+  appName: 'nori-daycare-app',
+  webDir: 'dist'
+};
+
+export default config;
